2dde88fc3066060749e460a08b252df883e602a6
dqvinhx <qvinh.dao@gmail.com>
2019-09-08 02:25:13 +0700

    link sang character

---
+++ project_Hieu/comic.js
@@ -0,0 +1,13 @@
+var colls=document.getElementsByClassName("collapsible");
+var i;
+for (i=0;i<colls.length;i++) {
+    colls[i].addEventListener("click",function() {
+        this.classList.toggle("active");
+        var content=this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
+    });
+};
